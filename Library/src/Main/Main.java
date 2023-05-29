e7e0474c9873c3c4aa7f438fc4008e988a6ab505
jiyeon2411 <daz128@naver.com>
2023-05-29 22:48:52 +0900

    수정

--- Library/src/Main/Main.java
+++ Library/src/Main/Main.java
@@ -14,8 +14,49 @@ public class Main {
 			System.out.println("1.도서 대출 | 2.도서 검색 | 3.대출 조회 | 4.회원 등록 | 5.종료");
 			System.out.println("-----------------------------------------------------");
 			System.out.println("선택> ");
-			int selectNo = scanner.nextInt();
+			int num = scanner.nextInt();
+			
+			switch (num) {
+			case 1: 
+				//대출
+				loan();
+				break;
+			case 2: 
+				//검색
+				searchBooks();
+				break;
+			case 3: 
+				//조회
+				checkLoan();
+				break;
+			case 4: 
+				//회원 등록
+				createAccount();
+				break;
+			case 5: 
+				run = false;
+				break;
+			default:
+				System.out.println("1~5사이의 숫자를 입력해주세요.");
+				break;
+			}
 		}
+	}
+
+	private static void createAccount() {
+		
+	}
+
+	private static void checkLoan() {
+		
+	}
+
+	private static void searchBooks() {
+		
+	}
+
+	private static void loan() {
+
 	}
 
 }
---
+++ Library/src/Object/Member.java
@@ -0,0 +1,6 @@
+package Object;
+
+public class Member {
+	String name;
+	
+}
